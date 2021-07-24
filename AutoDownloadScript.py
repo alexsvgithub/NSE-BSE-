33ffe279e39b71331f2250c03cec06086197dd0d
unknown <>
2021-07-25 00:28:48 +0530

    Final commit

---
+++ AutoDownloadScript.py
@@ -0,0 +1,19 @@
+import webbrowser
+import time
+import pandas as pd
+df=pd.read_csv("NIFTY100.csv")
+symbols = df['Symbol']
+print(symbols)
+lists = [x for x in symbols]
+print(lists)
+for i in lists:
+    #nse data
+    link='https://query1.finance.yahoo.com/v7/finance/download/'+i+'.NS?period1=1279929600&period2=1627084800&interval=1d&events=history&includeAdjustedClose=true'
+    print(link)
+    webbrowser.open(link, new=1)
+    time.sleep(0.5)
+    #bse data
+    link='https://query1.finance.yahoo.com/v7/finance/download/'+i+'.BO?period1=1279929600&period2=1627084800&interval=1d&events=history&includeAdjustedClose=true'
+    print(link)
+    webbrowser.open(link, new=1)
+print("DONE")
